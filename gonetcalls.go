b3f4d6df1c68143f5849cfd6862c5de7cbce162a
Saulius Grusnys <saulius.grusnys@shazam.com>
2015-09-22 14:31:27 +0100

    add support for GET headers

--- gonetcalls.go
+++ gonetcalls.go
@@ -47,9 +47,16 @@ func HttpPost(url string, headers map[string]string, data *bytes.Buffer) (code i
     return code, clen, rtt, body, resp.Header, ""
 }
 
-func HttpGet(url string, headers []string) (code int, clen, rtt int64, body string, rheaders http.Header, err_ret string) {
+func HttpGet(url string, headers map[string]string) (code int, clen, rtt int64, body string, rheaders http.Header, err_ret string) {
     startTime := time.Now()
-    resp, err := http.Get(url)
+
+    client := &http.Client{}
+    req, err := http.NewRequest("GET", url, nil)
+    for hname,hvalue := range(headers) {
+        req.Header.Add(hname, hvalue)
+    }
+	resp, err := client.Do(req)
+
     if err != nil {
         rtt = int64(time.Since(startTime).Nanoseconds()/1000)
         return 0, 0, rtt, "", nil, "Failed to get response from server." + err.Error()
